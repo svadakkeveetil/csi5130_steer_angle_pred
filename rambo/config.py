08a09ca37467821403d3a69fe6c1839103472e56
Sunish Vadakkeveetil <svadakkeveetil@oakland.edu>
2024-11-29 13:19:01 -0500

    uodates to predict and config

--- rambo/config.py
+++ rambo/config.py
@@ -1,22 +1,22 @@
 class DataConfig(object):
-    data_path = "/home/dhavan/AI_Project/Data/"
+    data_path = "/home/svadakkeveetil/ros/src/csi5130_steer_angle_pred/rambo/model_files/"
     data_name = "hsv_gray_diff_ch4"
     img_height = 192
     img_width = 256
     num_channels = 4
     
 class TrainConfig(DataConfig):
-    model_name = "comma_prelu"
-    batch_size = 4
+    model_name = "comma_6"
+    batch_size = 8
     epochs = 10
-    val_part = 6
-    X_train_mean_path = "/home/dhavan/AI_Project/Data/X_train_gray_diff2_mean.npy"
-    
+    val_part = 4
+    X_train_mean_path = "/home/svadakkeveetil/ros/src/csi5130_steer_angle_pred/rambo/data/X_train_gray_diff2_mean.npy"
 class TestConfig(TrainConfig):
-    model_path = "/home/dhavan/AI_Project/Data/final_model.hdf5"
+    model_path = "/home/svadakkeveetil/ros/src/csi5130_steer_angle_pred/rambo/checkpoints/final_model.hdf5"
     angle_train_mean = -0.004179079
+    image_path = "/home/svadakkeveetil/ros/src/csi5130_steer_angle_pred/rambo/"
 
 class VisualizeConfig(object):
-    pred_path = "submissions/final_model.csv"
+    pred_path = "submissions/final.csv"
     true_path = "/home/svadakkeveetil/ros/src/csi5130_steer_angle_pred/rambo/data/CH2_final_evaluation.csv"
     img_path = "phase2_test/center/*.jpg"
--- rambo/predict.py
+++ rambo/predict.py
@@ -10,11 +10,12 @@ data_path = config.data_path
 data_name = config.data_name
 val_part = config.val_part
 model_path = config.model_path
+image_path = config.image_path
 
-test_path = "{}/X_test_round2_{}.npy".format(data_path, data_name)
-test_path_phase1 = "{}/X_test_round1_{}.npy".format(data_path, data_name)
-out_name = model_path.replace("checkpoints", "submissions").replace("hdf5", "txt")
-out_name_phase1 = model_path.replace("checkpoints", "submissions").replace(".hdf5", "_phase1.txt")
+test_path = "{}X_test_round2_{}.npy".format(data_path, data_name)
+#test_path_phase1 = "{}X_test_round1_{}.npy".format(data_path, data_name)
+out_name = model_path.replace("checkpoints", "submissions").replace("hdf5", "csv")
+#out_name_phase1 = model_path.replace("checkpoints", "submissions").replace(".hdf5", "_phase1.txt")
 
 print("Loading model...")
 model = load_model(config.model_path)
@@ -37,33 +38,34 @@ dummy_preds = np.repeat(config.angle_train_mean, config.num_channels)
 preds = np.concatenate((dummy_preds, preds))
 
 # join predictions with frame_ids
-filenames = glob.glob("{}/round2/test/center/*.jpg".format(data_path))
+filenames = glob.glob("{}phase2_test/center/*.jpg".format(image_path))
 filenames = sorted(filenames)
-frame_ids = [f.replace(".jpg", "").replace("{}/round2/test/center/".format(data_path), "") for f in filenames]
+#frame_ids = [f.replace(".jpg", "").replace("{}/round2/test/center/".format(data_path), "") for f in filenames]
+frame_ids = [f.split('/')[-1].replace(".jpg", "") for f in filenames]
 
 print("Writing predictions...")
 pd.DataFrame({"frame_id": frame_ids, "steering_angle": preds}).to_csv(out_name, index=False, header=True)
 
-print("PHASE 1")
-print("Reading phase1 test data...")
-X_test = np.load(test_path_phase1)
-X_test = X_test.astype('float32')
-X_test -= X_train_mean
-X_test /= 255.0
+# print("PHASE 1")
+# print("Reading phase1 test data...")
+# X_test = np.load(test_path_phase1)
+# X_test = X_test.astype('float32')
+# X_test -= X_train_mean
+# X_test /= 255.0
 
-print("Predicting...")
-preds = model.predict(X_test)
-preds = preds[:, 0]
+# print("Predicting...")
+# preds = model.predict(X_test)
+# preds = preds[:, 0]
 
-dummy_preds = np.repeat(config.angle_train_mean, config.num_channels)
-preds = np.concatenate((dummy_preds, preds))
+# dummy_preds = np.repeat(config.angle_train_mean, config.num_channels)
+# preds = np.concatenate((dummy_preds, preds))
 
-# join predictions with frame_ids
-filenames = glob.glob("{}/round1/test/center/*.png".format(data_path))
-filenames = sorted(filenames)
-frame_ids = [f.replace(".png", "").replace("{}/round1/test/center/".format(data_path), "") for f in filenames]
+# # join predictions with frame_ids
+# filenames = glob.glob("{}round1/test/center/*.png".format(data_path))
+# filenames = sorted(filenames)
+# frame_ids = [f.replace(".png", "").replace("{}round1/test/center/".format(data_path), "") for f in filenames]
 
-print("Writing predictions...")
-pd.DataFrame({"frame_id": frame_ids, "steering_angle": preds}).to_csv(out_name_phase1, index=False, header=True)
+# print("Writing predictions...")
+# pd.DataFrame({"frame_id": frame_ids, "steering_angle": preds}).to_csv(out_name_phase1, index=False, header=True)
 
 print("Done!")
