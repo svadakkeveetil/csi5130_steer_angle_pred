af26b996e60944856d650b2792ab6b008a74c309
Sunish Vadakkeveetil <svadakkeveetil@oakland.edu>
2024-12-03 18:29:18 -0500

    updating the readme files and removing trial files

--- fileExtraction/steering_publisher.py
+++
@@ -1,8 +0,0 @@
-from dbw_mkz_msgs.msg import SteeringReport
-
-def callback():
-    # Send data using the SteeringReport message
-    msg = SteeringReport()
-    msg.steering_wheel_angle = 15.0
-    # Publish to the topic
-    publisher.publish(msg)
